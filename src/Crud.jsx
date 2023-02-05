083d753b2eced2c907f0472766a65686e379511b
Hein Htet Aung <heinhtetaung.sglife@gmail.com>
2023-02-05 13:04:46 +0700

    fix: add router

---
+++ src/Crud.jsx
@@ -0,0 +1,5 @@
+const Crud = () => {
+  return <h1>Crud</h1>;
+};
+
+export default Crud;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Crud from "./Crud";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "crud",
+    element: <Crud />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
